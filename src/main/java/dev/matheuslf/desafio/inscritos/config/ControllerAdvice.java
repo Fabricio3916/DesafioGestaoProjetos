6e030853a8a6336e6bcf3d3e9f980b4ceaff114b
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-10-31 10:54:31 -0300

    feat: implementa o ControllerAdvice para melhor resposta de erros

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/config/ControllerAdvice.java
@@ -0,0 +1,33 @@
+package dev.matheuslf.desafio.inscritos.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleEnumError(HttpMessageNotReadableException ex) {
+        Map<String, String> error = Map.of(
+                "status", "Valor inválido."
+        ); //melhorar mensagem de erro
+        return ResponseEntity.badRequest().body(error);
+    }
+
+}
