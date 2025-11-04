987682a4f57167b1f5bd377a0f39bfe572231d84
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-11-04 16:25:29 -0300

    feat: criação de classe para configuração do swagger e liberação de rotas no security

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package dev.matheuslf.desafio.inscritos.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI getOpenAPI() {
+
+        Contact contact = new Contact();
+        contact.setUrl("https://github.com/Fabricio3916");
+        contact.name("Fabricio Passos");
+
+
+        Info info = new Info();
+        info.title("Sistema de Gestão de Projetos");
+        info.version("v1");
+        info.description("Aplicação para gerenciamento de projetos de uma empresa");
+        info.contact(contact);
+
+        return new OpenAPI().info(info);
+    }
+
+}
