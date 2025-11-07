992600174d20a5a771b1b227095c1edd1a2c2ae8
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-11-07 08:47:20 -0300

    doc: refatora o SwaggerConfig com mais informações

--- src/main/java/dev/matheuslf/desafio/inscritos/config/SwaggerConfig.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/config/SwaggerConfig.java
@@ -1,36 +1,50 @@
 package dev.matheuslf.desafio.inscritos.config;
 
-
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
-        scheme = "bearer"
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI getOpenAPI() {
 
-        Contact contact = new Contact();
-        contact.setUrl("https://github.com/Fabricio3916");
-        contact.name("Fabricio Passos");
+        Contact contact = new Contact()
+                .name("Fabricio Passos")
+                .url("https://github.com/Fabricio3916")
+                .email("fabriciopassos3916@gmail.com");
 
-        Info info = new Info();
-        info.title("Sistema de Gestão de Projetos");
-        info.version("v1");
-        info.description("Aplicação para gerenciamento de projetos de uma empresa");
-        info.contact(contact);
+        Info info = new Info()
+                .title("Sistema de Gestão de Projetos")
+                .version("v1")
+                .description("""
+                        API para gerenciamento de projetos e tarefas de uma empresa.
+                        Autenticação via JWT e operações CRUD seguras para usuários e projetos.
+                        """)
+                .contact(contact)
+                .license(new License()
+                        .name("MIT License")
+                        .url("https://opensource.org/licenses/MIT"));
 
-        return new OpenAPI().info(info);
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Servidor Local")
+                ));
     }
-
 }
